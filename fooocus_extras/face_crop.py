5aad807986030ae2a8c56f22bf76db4e53143225
Narek <153336357+bertuop@users.noreply.github.com>
2023-12-29 20:30:31 +0100

    Update face_crop.py

--- fooocus_extras/face_crop.py
+++ fooocus_extras/face_crop.py
@@ -20,6 +20,23 @@ def align_warp_face(self, landmark, border_mode='constant'):
                                   borderMode=border_mode, borderValue=(135, 133, 132))
     return cropped_face
 
+faceRestoreHelper = None
+
+
+def align_warp_face(self, landmark, border_mode='constant'):
+    affine_matrix = cv2.estimateAffinePartial2D(landmark, self.face_template, method=cv2.LMEDS)[0]
+    self.affine_matrices.append(affine_matrix)
+    if border_mode == 'constant':
+        border_mode = cv2.BORDER_CONSTANT
+    elif border_mode == 'reflect101':
+        border_mode = cv2.BORDER_REFLECT101
+    elif border_mode == 'reflect':
+        border_mode = cv2.BORDER_REFLECT
+    input_img = self.input_img
+    cropped_face = cv2.warpAffine(input_img, affine_matrix, self.face_size,
+                                  borderMode=border_mode, borderValue=(135, 133, 132))
+    return cropped_face
+
 
 def crop_image(img_rgb):
     global faceRestoreHelper
